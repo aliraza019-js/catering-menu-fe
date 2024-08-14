19ee02e593512df25dc3868a84d9e7c5de3a8c58
Ali Raza <admin@admins-MacBook-Air.local>
2024-08-15 00:29:26 +0500

    enhance improvements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,21 +7,46 @@
     </v-row>
     <v-row>
       <v-col :cols="$vuetify.breakpoint.mdAndUp ? 8 : 12">
+        <!-- <Location />
+        <TopSellers /> -->
         <Menu />
+        <!-- <Contactus /> -->
+        <!-- <Delivery id="delivery-module" :totalAmountData="deliveryTotalData"/> -->
+        <!-- Content goes here -->
       </v-col>
-      <v-col v-show="$vuetify.breakpoint.mdAndUp" cols="4">
+      <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="4">
         <div class="sticky-wrapper">
-          <CheckoutCart @move-to-delivery-module="moveToDelivery" />
+          <CheckoutCart @move-to-delivery-module="moveToDelivery($event)"/>
         </div>
       </v-col>
     </v-row>
-
-    <v-badge v-show="$vuetify.breakpoint.smAndDown" :content="cartItemCount" color="white" overlap>
-      <v-btn class="checkout-fab" color="primary" dark fab bottom right @click="checkoutDialog = true">
+    <!-- <v-row>
+      <v-col cols="12">
+        <Footer />
+      </v-col>
+    </v-row> -->
+    
+    <!-- Floating Action Button for mobile view -->
+    <v-badge
+      v-if="$vuetify.breakpoint.smAndDown"
+      :content="cartItemCount"
+      color="white"
+      overlap
+    >
+      <v-btn
+        class="checkout-fab"
+        color="primary"
+        dark
+        fab
+        bottom
+        right
+        @click="checkoutDialog = true"
+      >
         <v-icon>mdi-cart</v-icon>
       </v-btn>
     </v-badge>
 
+    <!-- Checkout Dialog -->
     <v-dialog v-model="checkoutDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
       <v-card>
         <v-toolbar dark color="primary">
@@ -31,7 +56,7 @@
           <v-toolbar-title>Checkout Cart</v-toolbar-title>
         </v-toolbar>
         <v-card-text class="px-0">
-          <CheckoutCart @move-to-delivery-module="moveToDelivery" />
+          <CheckoutCart @move-to-delivery-module="moveToDelivery($event)"/>
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -39,31 +64,46 @@
 </template>
 
 <script>
-const Hero = () => import("../components/Hero");
-const Menu = () => import("../components/menu");
-const CheckoutCart = () => import("@/components/CheckoutCart");
+import Hero from "../components/Hero";
+import Location from "../components/Location";
+import TopSellers from "../components/topSellers";
+import Menu from "../components/menu";
+import CheckoutCart from "@/components/CheckoutCart";
+import Contactus from "@/components/contactus";
+import Delivery from "@/components/DeliveryModule";
+import Footer from "@/components/Footer";
 
 export default {
   name: "Home",
   components: {
     Hero,
+    Location,
+    TopSellers,
     Menu,
     CheckoutCart,
+    Contactus,
+    Delivery,
+    Footer,
   },
-  data: () => ({
-    checkoutDialog: false,
-  }),
+  data() {
+    return {
+      deliveryTotalData: {},
+      checkoutDialog: false,
+    }
+  },
   computed: {
     cartItemCount() {
       return this.$store.getters.cartItems.length;
     }
   },
   methods: {
-    moveToDelivery() {
-      const deliveryModule = document.getElementById("delivery-module");
-      if (deliveryModule) {
-        deliveryModule.scrollIntoView({ behavior: "smooth" });
-      }
+    moveToDelivery(data) {
+      window.scrollTo({
+        top: document.getElementById("delivery-module").offsetTop,
+        left: 0,
+        behavior: "smooth",
+      });
+      this.deliveryTotalData = data;
     }
   }
 };
@@ -71,8 +111,9 @@ export default {
 
 <style>
 .sticky-wrapper {
+  position: -webkit-sticky;
   position: sticky;
-  top: 0;
+  top: 0; /* Adjust this value as needed */
 }
 
 .checkout-fab {
